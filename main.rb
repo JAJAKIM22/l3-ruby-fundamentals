0aaa600c7b1a825df6ff5c185e3ba3b78088d576
Peter Kimani <peterjajakim@gmail.com>
2022-08-16 13:02:06 +0300

    PRACTICE

--- main.rb
+++ main.rb
@@ -2,19 +2,25 @@
 
 # 1.0 Printing Methods
 # TODO: puts
-
+puts "My name is Peter"
+puts "My name is Peter"
 # TODO: print
-
+print "HELLO"
+print "HELLO"
 # TODO: pp
-
+pp [{ id: 1, hello: "World" }]
 # 2.0 Strings
 # TODO: CREATE A STRING
-
+puts "I am a coder"
 # TODO: CREATE A STRING WITH SINGLE QUOTES
-
+puts 'i am a coder'
 # TODO: INTERPOLATE STRINGS
 
 # TODO: String methods (upcase, downcase, capitalize)
+new_wave = "new".capitalize
+puts new_wave
+puts new_wave = "new".downcase
+puts new_wave = "new".upcase
 
 # 3.0 Numbers
 # TODO: Integers
@@ -22,27 +28,38 @@
 # TODO: Float
 
 # TODO: Number methods (floor, ceil, to_i, to_f)
-
-
+puts 50.5.floor
+puts 50.5.ceil
+puts 50.5.to_i
+puts 50.5.to_f
 # 4.0 Boolean
 # TODO: True / Truthy
 
 # TODO: False / Falsy
-
+isTall = false
+puts isTall
 # 5.0 Symbols
 # TODO: Create symbol
-
-
+tree = :Moringa
+puts tree
+pp tree
 # 6.0 Arrays
 # TODO: Create array natively
 
 # TODO: Create array using Array class
-
+roads = Array.new
+roads.push("Great North Road")
+pp roads
 # 7.0 Hashes
 # TODO: Create Hash
-
+user = {name:"Peter", age:18 }
+pp user
+pp user[:name]
 # TODO: Create Hash using Hash class
-
+student = Hash.new
+puts student
+student[:name] = "Peter"
+puts student
 # 8.0 Methods
 # TODO: Method with enclosed params
 
